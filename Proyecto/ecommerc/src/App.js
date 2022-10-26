f331bf425d66894cfa1eb18bcc2c802bb48c302a
Bautista Ortiz Aury <bauti707@gmail.com>
2022-10-26 01:07:39 -0300

    Rindiendome con react

--- Proyecto/ecommerc/src/App.js
+++ Proyecto/ecommerc/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import ItemListCotainer from './containers/ItemListCotainer'
+import ItemListCotainer from './ItemListCotainer'
 function App() {
   return (
     <div className="App">
--- Proyecto/ecommerc/src/containers/ItemListContainer/index2.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import './style2.css';
-function ItemListContainer ({greeting}) {
-    return (
-        <div className='item-list-container'>
-            <h2>{greeting}</h2>
-        </div>
-    )
-}
